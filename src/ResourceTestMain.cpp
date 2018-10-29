4031298f50313f30a4eaf17a76e9809b9619a275
Jake McLeman <jake@mcleman.com>
2018-10-29 04:06:12 +0000

    Made it build, renamed unnamed engine to downpour engine

--- src/ResourceTestMain.cpp
+++ src/ResourceTestMain.cpp
@@ -1,9 +1,12 @@
 #include "Resource.h"
+#include "BaseTypes.h"
 
+#include <string>
+#include <vector>
 #include <iostream>
 
-using namespace UnnamedEngine;
-using namespace UnnamedEngine::Types;
+using namespace Downpour;
+using namespace Downpour::Types;
 
 class TestResource : public Resource<std::string>
 {
@@ -11,29 +14,29 @@ class TestResource : public Resource<std::string>
     TestResource(const std::string& id) : Resource<std::string>(id) {}
 
   protected:
-    void LoadInternal(std::vector<byte>&& aBytes) override
+    bool LoadInternal(std::vector<byte>&& aBytes) override
     {
       std::cout << GetIdentifier() << ": ";
-      for(byte : aBytes)
+      for(byte b : aBytes)
       {
-        std::cout << static_cast<char>(byte);
+        std::cout << static_cast<char>(b);
       }
       std::cout << std::endl;
       return true;
     }
 
-    void UnloadInternal() override
+    bool UnloadInternal() override
     {
       std::cout << "Unloaded" << std::endl;
       return true;
     }
-}
+};
 
 int main(int argc, char** argv)
 {
   TestResource tr("test");
 
   std::string toReq = "butts lol";
-
-  tr.load(toReq);
+  std::vector<byte> bytes(toReq.begin(), toReq.end());
+  tr.Load(std::move(bytes));
 }
