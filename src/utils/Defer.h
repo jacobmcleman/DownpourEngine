b5f0daa53cdcf3a0112acc059118309934ddeebd
jacobmcleman <j.mcleman@digipen.edu>
2019-01-25 20:15:50 -0800

    Removed garbage from defer.h

--- src/utils/Defer.h
+++ src/utils/Defer.h
@@ -1,21 +1,6 @@
 #ifndef _DEFER_H
 #define _DEFER_H
 
-#include "CompileTimeCounter.h"
-
-namespace Deference
-{
-  class Deferrer
-  {
-  public:
-    Deferrer(void (*toDefer)()) : deferred(toDefer) {}
-    ~Deferrer() { deferred(); }
-
-  private:
-    void (*deferred)();
-  };
-}
-
 #define DEFER(statement) \
   struct Deferrer \
   {\
