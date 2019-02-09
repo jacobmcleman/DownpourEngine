4a986e1d91a9c09d8e64f88473b998d2e4850472
jacobmcleman <j.mcleman@digipen.edu>
2019-02-08 22:56:40 -0800

    I exist solely to please CodeFactor

--- src/ResourceManager.h
+++ src/ResourceManager.h
@@ -15,7 +15,6 @@ namespace Downpour
     public:
       ResourceManager()
       {
-
       }
 
       ~ResourceManager()
--- src/utils/Singleton.h
+++ src/utils/Singleton.h
@@ -27,7 +27,6 @@ namespace Downpour
     public:
       Singleton()
       {
-        
       }
 
       static T& GetInstance() 
